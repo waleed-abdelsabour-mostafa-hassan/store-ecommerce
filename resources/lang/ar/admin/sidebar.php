586593ea1a82580bc5b77915dcc3ea5a59ea56f6
waleed-abdelsabour-mostafa-hassan <waleedabdoh11@gmail.com>
2021-06-16 10:49:26 +0200

    begin branch

---
+++ resources/lang/ar/admin/sidebar.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+
+    'settings'  => 'الأعدادات',
+    'shipping methods' => 'وسائل التوصيل',
+    'free shipping' => ' توصيل مجانى',
+    'inner shipping' => ' توصيل داخلى',
+    'outer shipping' => ' توصيل خارجى',
+
+
+
+];
---
+++ resources/lang/en/admin/sidebar.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+    'settings'  => 'Settings',
+    'shipping methods' => 'Shipping Methods',
+    'free shipping' => 'Free Shipping',
+    'inner shipping' => 'Inner Shipping',
+    'outer shipping' => 'Outer Shipping',
+];
